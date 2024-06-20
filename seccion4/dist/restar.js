7905bda93c3a237e325fc930a23f0809266c5b5f
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2024-06-20 03:03:59 -0600

    seccion4 completada

---
+++ seccion4/dist/restar.js
@@ -0,0 +1,3 @@
+"use strict";
+const restar = (a, b) => a - b;
+console.log(restar(24, 4));
---
+++ seccion4/src/multiplicar.ts
@@ -0,0 +1,2 @@
+const multiplicar = (a : number, b : number) : number => a * b;
+console.log(multiplicar(2,3));
