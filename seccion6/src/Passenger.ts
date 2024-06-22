866d7cdedc4012797f1b6f157723ef3794914f73
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2024-06-22 03:03:49 -0600

    seccion6 avance

---
+++ seccion6/src/Passenger.ts
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker';
+
+export class Passenger{
+    private readonly name : string;
+    private readonly location : {
+        lat : number,
+        lng : number
+    }
+
+    constructor(){
+        this.name = faker.person.firstName();
+        this.location = {
+            lat: faker.location.latitude(),
+            lng: faker.location.longitude()
+        }
+    }
+}
---
+++ seccion6/src/index.ts
@@ -0,0 +1,3 @@
+import { Passenger } from "./Passenger";
+const ejemplo = new Passenger();
+console.log(ejemplo);
